add3a2622650942d6ca33f4288c251f85e86db0b
Daniel <pepelacon@gmail.com>
2023-08-09 08:12:09 -0400

    add models for exercise and workout

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import morgan from 'morgan'
-
 import 'colors'
-import authRoutes from './app/auth/auth.routes.js'
-import usersRoutes from './app/user/user.routes.js'
-import { prisma } from './app/prisma.js'
+import dotenv from 'dotenv'
+import express from 'express'
+import morgan from 'morgan'
+import path from 'path'
+
 import { errorHandler, notFound } from './app/middleware/error.middleware.js'
+
+import authRoutes from './app/auth/auth.routes.js'
+import exerciseRoutes from './app/exercise/exercise.routes.js'
+import { prisma } from './app/prisma.js'
+import userRoutes from './app/user/user.routes.js'
 
 dotenv.config()
 
@@ -14,18 +17,26 @@ const app = express()
 
 async function main() {
 	if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
+
 	app.use(express.json())
+
+	const __dirname = path.resolve()
+
+	app.use('/uploads', express.static(path.join(__dirname, '/uploads/')))
+
 	app.use('/api/auth', authRoutes)
-	app.use('/api/users', usersRoutes)
+	app.use('/api/users', userRoutes)
+	app.use('/api/exercises', exerciseRoutes)
 
 	app.use(notFound)
 	app.use(errorHandler)
+
 	const PORT = process.env.PORT || 5000
 
 	app.listen(
 		PORT,
 		console.log(
-			`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green
+			`🚀 Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue
 				.bold
 		)
 	)
